e7c3ba25fd75bafced016ab0afb58fa9b7950281
Takahiro <qwerty@suishin.xyz>
2018-07-07 17:34:59 +0000

    ログイン機能追加3

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :articles do
+    
     collection do
       post :confirm
     end
